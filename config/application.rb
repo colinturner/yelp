622e1c07b9e57ed2b6145026855b285efd51ba15
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-05-02 14:08:45 +0100

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# config.generators do |g|
+#   g.test_framework :rspec
+# end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)

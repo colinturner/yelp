76f32666bbfd1af0bc59fcfe7593cd9c604ed8e0
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-05-02 16:41:18 +0100

    exercise 12 - work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502111413) do
+ActiveRecord::Schema.define(version: 20170502153608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20170502111413) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "thoughts"
+    t.integer  "rating"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end

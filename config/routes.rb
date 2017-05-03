c816c9d1b55b39c5413f9bdc22a1e7347675d78c
Colin Turner <colinturner34@gmail.com>
2017-05-03 15:30:54 +0100

    User can log in using Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'restaurants#index'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

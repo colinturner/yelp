828281d858bc683b2eb6843b54f9696def47f082
Colin Turner <colinturner34@gmail.com>
2017-05-03 11:12:47 +0100

    validates rating is between 1 and 5 + Users cannot submit the same restaurant description twice

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,15 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
